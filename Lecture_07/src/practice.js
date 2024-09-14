8622db16f7e66a238ae4b074c2c3f62d3fe3490a
Mahendra <mahendrapurohit9889@gmail.com>
2024-09-14 18:54:18 +0530

    Dom

---
+++ Lecture_07/src/practice.js
@@ -0,0 +1,61 @@
+
+
+// Attributes
+
+//   let att =  document.querySelector("h1");
+//   let clas = document.querySelector("div");
+//   console.log(att);
+
+//   let val = att.getAttribute("id");
+//   console.log(val);
+
+//   att.setAttribute("id", "hello");
+
+    //  clas.setAttribute("class","proper");
+
+
+
+    // create new  Element 
+
+  
+// let newBtn = document.createElement("button");
+// newBtn = innertext = "Click me";    
+
+let div = document.querySelector(".main");
+
+// div.append(newBtn);
+
+
+
+
+
+// create New Element 
+
+//   let D =  document.createElement("div");
+//    D.innerText = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod sint at vitae asperiores pariatur reiciendis officia, ea esse sit optio."
+//   div.append(D);
+
+// create new btn 
+
+   let btn  = document.createElement("button");
+
+//    console.log(btn)
+
+//    btn.innerText= "Click Here"
+
+//    div.append(btn)
+
+
+// let Pbtn = document.createElement("button");
+
+// Pbtn.innerText = "Click Me"
+
+// div.prepend(Pbtn)
+
+// div.before(Pbtn);   
+
+// div.after(Pbtn)
+
+
+// let p = document.querySelector(div);
+    
---
+++ Lecture_07/src/script.js
@@ -0,0 +1,33 @@
+
+/*
+        //  DOM manipulation : Attributes
+
+            getAttributes(attr)          // to get attributes value 
+            setAttributes(attr,value)    //  to set the attributes val th  
+            classList.add .remove   // to add new class and remove class 
+         
+
+
+
+          DOM Style 
+          
+            document.body.style.visibility = "hidden"
+            val.style.display = "none"
+            document.body.style.color = "red"
+            document.body.style.backgroundColor = "red"
+            
+
+           DOM Insert Elements
+
+            document.createaElement("button")                 //   append for starting apply   // pepend for ending apply  
+            node.append(el) // adds at the end of nods(inside ) 
+            node.prepend(el)  // adds at the start of node (inside)
+            node.before(el)  // add before the node (outside)
+            node.after(el)   //  add after the node(outside) 
+
+
+           
+           
+
+
+*/    
