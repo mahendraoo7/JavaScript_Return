cc866b9e4a7d0a004bd29e29ba164989a7c1c4dc
Mahendra <mahendrapurohit9889@gmail.com>
2024-09-20 18:45:21 +0530

    Complate Js

---
+++ Lecture_12/Script.js
@@ -0,0 +1,21 @@
+/*
+
+    // API - Application Program InterFace
+
+//  Fetch Api
+   -> fetch api is providing an interface for fatching (sending / reciving) resources.
+   
+    -> response request cycle.
+
+   //   Understanding Terms 
+   
+     AJAX - is asynchronous js and XML
+     JSON - is javascript Object Notation
+
+     Json() method.
+      ->  return a secound promises that resolve with the result of parsing response body text as json. (input is json output is json obj) 
+
+     HTTP - hyper text transfar protocol
+
+*/
+
